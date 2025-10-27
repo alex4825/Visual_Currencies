6c2b3dc6e93f6dadb9621b49d6e25f67f4f70889
Alexey Jukov <nitroalexus@gmail.com>
2025-10-27 22:54:07 +0600

    Currency range config + validation

---
+++ Assets/_Project/Develop/Runtime/Configs/CurrencyRangeConfig.cs
@@ -0,0 +1,40 @@
+﻿using Assets._Project.Develop.Runtime.Meta.Features.Wallet;
+using AYellowpaper.SerializedCollections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets._Project.Develop.Runtime.Configs
+{
+    [CreateAssetMenu(fileName = "CurrencyRangeConfig", menuName = "Configs/CurrencyRangeConfig")]
+    public class CurrencyRangeConfig : ScriptableObject
+    {
+        [SerializeField]
+        [SerializedDictionary("Currency", "Range")]
+        private SerializedDictionary<CurrencyTypes, Vector2Int> _values;
+
+        public Vector2Int GetRangeFor(CurrencyTypes currencyType) => _values[currencyType];
+
+        public void OnValidate()
+        {
+            var keys = new List<CurrencyTypes>(_values.Keys);
+
+            foreach (var key in keys)
+            {
+                Vector2Int currentValue = _values[key];
+                Vector2Int newValue = currentValue;
+
+                if (newValue.x < 0)
+                    newValue.x = 0;
+
+                if (newValue.y < 0)
+                    newValue.y = 0;
+
+                if (newValue.x > newValue.y)
+                    newValue.x = newValue.y;
+
+                if (newValue != currentValue)
+                    _values[key] = newValue;
+            }
+        }
+    }
+}
